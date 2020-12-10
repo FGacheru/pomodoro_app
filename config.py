4a398f0849f22b868d47b3f2e3de71a803f5c229
b-ndunda <francisgacheru2001@gmail.com>
2020-12-10 22:26:20 +0300

    add a form with the timer

--- config.py
+++ config.py
@@ -1,26 +1,13 @@
 import os
 
-#  email configurations
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
 class Config:
     """
     general configuration parent class
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-    
-    
+   
+
+
+
 class ProdConfig(Config):
     """
     production configuration child clas
@@ -28,12 +15,8 @@ class ProdConfig(Config):
     Args:
        config: The parent configuration class with general configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
-    
-    
-    
-    
 class DevConfig(Config):
     """
     development configuration child class
@@ -41,16 +24,12 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings.
     """
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches'
-    DEBUG = True   
+    DEBUG = True
     
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches_test'
     
-       
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-'test':TestConfig
-}    
-    
+# 'test':TestConfig
+
+}
